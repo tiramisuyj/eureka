e6f84fd5ae43f18e5dcc451ebba5ddb4e0de6683
tiramisuyj <tiramisuyj@163.com>
2019-05-08 16:25:23 +0800

    初始化代码，提供springboot eureka负载均衡功能

---
+++ client1/src/main/java/com/demo/eureka/client1/Client1Application.java
@@ -0,0 +1,39 @@
+package com.demo.eureka.client1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Eureka客户端
+ */
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class Client1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Client1Application.class, args);
+    }
+
+    /**
+     * 假如这个客户端要提供一个getUser的方法
+     * @return
+     */
+    @GetMapping(value = "/getUser")
+    @ResponseBody
+    public Map<String,Object> getUser(@RequestParam Integer id){
+        Map<String,Object> data = new HashMap<>();
+        data.put("id",id);
+        data.put("userName","admin");
+        data.put("from","provider-A");
+        return data;
+    }
+}
